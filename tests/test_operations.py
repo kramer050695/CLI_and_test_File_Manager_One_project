736070e7a9cd47608ad603774690af90e17cf6b1
Zaguzov <kramer050695@gmail.com>
2025-05-16 10:23:40 +0300

    Adding Libraries

---
+++ tests/test_operations.py
@@ -0,0 +1,12 @@
+import unittest
+import tempfile
+import shutil
+import os
+from operations import (
+     copy_file,
+     delete_path,
+     count_files_in_dir,
+     find_files_in_dir,
+     add_date_to_files,
+     _add_date_to_filename
+)
